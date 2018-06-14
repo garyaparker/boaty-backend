0062b9ad7888f525953d2a2156e2a66f55493988
GARY PARKER <Gary.Parker@libertymutual.com>
2018-06-14 19:49:53 -0400

    Setup connection debuging.

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const app = express();
 
 const dbHost = process.env.DB_HOST ? process.env.DB_HOST : 'localhost';
 
-mongoose.connect(`mongodb://${dbHost}/boaty`)
-  .then(() => console.log(`Connected to database ${dbHost}`))
-  .catch(console.error);
+app.use((req, res) => {
+  mongoose.connect(`mongodb://${dbHost}/boaty`)
+    .then(() => res.send('Connected'))
+    .catch((error) => res.send(error));
+});
 
-app.use((req, res) => res.send('hello boats'));
 app.listen(process.env.PORT || 3000);
