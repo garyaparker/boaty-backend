6f4925b7fea208f72444a8842681dbe53080064c
GARY PARKER <Gary.Parker@libertymutual.com>
2018-06-14 19:15:15 -0400

    Added db connection test.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-app.use((req, res) => res.send('hello boats'));
 
+const dbHost = process.env.NODE_ENV === 'production' ? '172.31.19.147' : 'localhost';
+
+mongoose.connect(`mongodb://${dbHost}/boaty`)
+  .then(() => console.log(`Connected to database ${dbHost}`))
+  .catch(console.error);
+
+app.use((req, res) => res.send('hello boats'));
 app.listen(process.env.PORT || 3000);
