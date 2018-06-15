d555974f2488032e93871130d69be8574481692e
GARY PARKER <Gary.Parker@libertymutual.com>
2018-06-14 21:49:05 -0400

    Test S3 connection.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+var aws = require('aws-sdk');
 
 const app = express();
+const s3 = new aws.S3();
 
 const dbHost = process.env.DB_HOST ? process.env.DB_HOST : 'localhost';
+
+app.get('/api/faces', (req, res) => {
+  const params = { Bucket: 'boaty-faces', MaxKeys: 2 };
+  s3.listObjectsV2(params, (err, data) => err ? res.send(err) : res.send(data));
+});
 
 app.use((req, res) => {
   mongoose.connect(`mongodb://${dbHost}/boaty`)
