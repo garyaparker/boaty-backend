5c9bff2bd8a2e571a8e7a554c766cfdfc80c52d7
GARY PARKER <Gary.Parker@libertymutual.com>
2018-06-15 06:57:24 -0400

    Added error logging and nodemon.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const aws = require('aws-sdk');
+const logger = require('./utils/logger');
 
 const app = express();
 const s3 = new aws.S3();
@@ -11,16 +12,22 @@ const dbHost = process.env.DB_HOST ? process.env.DB_HOST : 'localhost';
 app.get('/', (req, res) => res.send());
 
 // S3 connection test
-app.get('/api/faces', (req, res) => {
+app.get('/api/s3-test', (req, res) => {
   const params = { Bucket: 'boaty-faces', MaxKeys: 2 };
   s3.listObjectsV2(params, (err, data) => err ? res.send(err) : res.send(data));
 });
 
 // MongoDB connection test
-app.use((req, res) => {
+app.get('/api/db-test', (req, res) => {
   mongoose.connect(`mongodb://${dbHost}/boaty`)
     .then(() => res.send('Connected'))
     .catch((error) => res.send(error));
+});
+
+// Error handler
+app.use((error, req, res, next) => { // eslint-disable-line no-unused-vars
+  logger.error(error);
+  res.status(500).send(error.stack);
 });
 
 app.listen(process.env.PORT || 3000);
