a1c08195de9b94d6370bc9cfb0d027aacd275f51
GARY PARKER <Gary.Parker@libertymutual.com>
2018-06-15 06:12:26 -0400

    Added health check and linting.

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-var aws = require('aws-sdk');
+const aws = require('aws-sdk');
 
 const app = express();
 const s3 = new aws.S3();
 
 const dbHost = process.env.DB_HOST ? process.env.DB_HOST : 'localhost';
 
+// Health check
+app.get('/', (req, res) => res.send());
+
+// S3 connection test
 app.get('/api/faces', (req, res) => {
   const params = { Bucket: 'boaty-faces', MaxKeys: 2 };
   s3.listObjectsV2(params, (err, data) => err ? res.send(err) : res.send(data));
 });
 
+// MongoDB connection test
 app.use((req, res) => {
   mongoose.connect(`mongodb://${dbHost}/boaty`)
     .then(() => res.send('Connected'))
